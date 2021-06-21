240f83fcdafff31ec64615fb6e06964538bae1fe
wping <wen240979@gmail.com>
2021-06-21 21:02:04 +0800

    conditional rendering of movies component.

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,37 +7,43 @@ function Movies() {
     const deleted_movies = movies.filter((m) => m._id !== movie._id);
     setMovies(deleted_movies);
   };
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Genre</th>
-          <th>Stock</th>
-          <th>Rate</th>
-          <th></th>
-        </tr>
-      </thead>
 
-      <tbody>
-        {movies.map((movie) => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <button
-                className="btn btn-danger btn-sm"
-                onClick={() => handleDelete(movie)}
-              >
-                Delete
-              </button>
-            </td>
+  const { length: count } = movies;
+  if (count === 0) return <p>There are no movies in the database.</p>;
+  return (
+    <>
+      <p>Showing {count} movies in the database.</p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleDelete(movie)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
